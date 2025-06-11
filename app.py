799fa4a228dcf1a78519b7c31c8b58d5dfbd1689
Umer Mala <ufarooq90.uf@gmail.com>
2025-06-11 16:28:27 +0500

    added

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Success! Your app is running inside Docker."
+    db_password = os.getenv("DB_PASSWORD", "Not Set")
+    api_key = os.getenv("API_KEY", "Not Set")
+    return (
+        f"Success! Your app is running inside Docker.<br>"
+        f"DB_PASSWORD: {db_password}<br>"
+        f"API_KEY: {api_key}"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
